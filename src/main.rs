81e5a8b8afe67c3108a3d54a4278d383d7ead0e4
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2024-03-06 15:24:16 +0100

    Ferris says!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod mod_use;
+
+use ferris_says::say; // from the previous step
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, Rust!");
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
 }
